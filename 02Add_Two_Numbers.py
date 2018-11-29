f10cf99bf00265e6a1e83c9a3ea2087185757e97
jediL <liujadi@gmail.com>
2018-11-29 12:46:44 +0800

    181129

--- 02Add_Two_Numbers.py
+++ 02Add_Two_Numbers.py
@@ -1,10 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
 class Solution:
     def addTwoNumbers(self, l1, l2):
         """
@@ -12,7 +5,7 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        # n is the node of l1
+
         n = l1
         i = 1
         num_l1 = 0
@@ -22,7 +15,6 @@ class Solution:
             i = i * 10
             n = n.next
 
-        # m is the node of l2
         m = l2
         j = 1
         num_l2 = 0
@@ -31,26 +23,12 @@ class Solution:
             num_l2 = num_l2 + m.val * j
             j = j * 10
             m = m.next
-        # get the sum of l1 , l2
+
         str_num = str(num_l1 + num_l2)
-        # reverse str_num
         str_num = str_num[::-1]
-        # turn to list output
-        list_result = []
+        res = list_result = ListNode(0)
+
         for s in str_num:
-            list_result.append(int(s))
-        return list_result
-
-
-# test code
-l1 = ListNode(2)
-l1.next = ListNode(4)
-l1.next.next = ListNode(3)
-
-l2 = ListNode(5)
-l2.next = ListNode(6)
-l2.next.next = ListNode(4)
-
-s = Solution()
-result = s.addTwoNumbers(l1, l2)
-print(result)
+            list_result.next = ListNode(int(s))
+            list_result = list_result.next
+        return res.next
